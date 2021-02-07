e52535bca950868cd1816baab9534d07ea0420c2
hanjiangtao <h15968804551@gmail.com>
2021-02-07 15:25:36 +0000

    do something new

--- common/log.h
+++ common/log.h
@@ -1,10 +1,51 @@
 #ifndef __LOG_H_
 #define __LOG_H_
+#include "stdio.h"
+#include "stdlib.h"
 
-#define LOG_DBG()
-#define LOG_INFO()
-#define LOG_WARN()
-#define LOG_ERR()
-#define LOG_EVENT()
+enum class LogLevel : uint8_t {
+    debug = 0,
+    info,
+    warn,
+    error,
+    event,
+    null
+};
+
+static LogLevel LOG_EVEL = LogLevel::error;
+
+#define LOG_COMMON(format, ...) do {    \
+    printf(format, ##__VA_ARGS__);      \
+} while (false);                        \
+
+#define LOG_DBG(format, ...) do {                                                                   \
+    if (LOG_EVEL <= LogLevel::debug) {                                                              \
+        LOG_COMMON("[DEBUG] [%s: %d][Tid: %d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);    \
+    }                                                                                               \
+} while (false);                                                                                    \
+
+#define LOG_INFO(format, ...) do {                                                                  \
+    if (LOG_EVEL <= LogLevel::info) {                                                               \
+        LOG_COMMON("[INFO] [%s: %d][Tid: %d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);     \
+    }                                                                                               \
+} while (false);                                                                                    \
+
+#define LOG_WARN(format, ...) do {                                                                  \
+    if (LOG_EVEL <= LogLevel::warn) {                                                               \
+        LOG_COMMON("[WARN] [%s: %d][Tid: %d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);     \
+    }                                                                                               \
+} while (false);                                                                                    \
+
+#define LOG_ERR(format, ...) do {                                                                   \
+    if (LOG_EVEL <= LogLevel::error) {                                                              \
+        LOG_COMMON("[ERROR] [%s: %d][Tid: %d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);    \
+    }                                                                                               \
+} while (false);                                                                                    \
+
+#define LOG_EVENT(format, ...) do {                                                                 \
+    if (LOG_EVEL <= LogLevel::event) {                                                              \
+        LOG_COMMON("[EVENT] [%s: %d][Tid: %d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);    \
+    }                                                                                               \
+} while (false);                                                                                    \
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "main.h"
+#include "common/common.h"
+
+extern LogLevel LOG_EVEL;
 
 int main(void)
 {
+    LOG_EVEL = LogLevel::warn;
+    LOG_INFO("hahah%d", 1);
     return 0;
 }
